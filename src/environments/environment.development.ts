e00da344e1d37b30a07ef33bfab248a52b450e9e
qasim <qasimhasanbilgrami1@gmail.com>
2024-10-09 18:18:12 +0500

    checking versal

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,6 @@
 export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:8000/',
+  //production: false,
+  production: true,
+  //apiUrl: 'http://localhost:8000/',
+ apiUrl:'https://fastapi-gkb04u0xx-qasims-projects-b6e78199.vercel.app/',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
-  apiUrl: "",
+  apiUrl:'https://fastapi-gkb04u0xx-qasims-projects-b6e78199.vercel.app/',
+  //apiUrl:'',
   production: false,
 };
