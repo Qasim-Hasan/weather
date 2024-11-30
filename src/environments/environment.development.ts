53d11cfd5d0f0066faf6b801d90f180b702fef4f
Qasim Hasan <qasimhasanbilgrami1@gmail.com>
2024-11-30 11:24:57 +0500

    Update environment.development.ts

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false,
   //apiUrl: 'http://localhost:8000/',
   secretPin: '111',
-  apiUrl: 'http://13.200.237.11:8000/'
+  apiUrl: 'http://13.233.246.223:8000/'
 };
