02a618132621d8e57320f8b9970ec5f4e4f4780e
Qasim Hasan <qasimhasanbilgrami1@gmail.com>
2024-12-01 18:34:49 +0500

    Update environment.development.ts

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false,
   //apiUrl: 'http://localhost:8000/',
   secretPin: '111',
-  apiUrl: 'http://13.233.246.223:8000/'
+  apiUrl: 'https://qvhess996f.execute-api.ap-south-1.amazonaws.com/'
 };
